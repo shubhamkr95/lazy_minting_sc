435910d758746b0faecb620520941f38515eb9bf
shubhamkr95 <sahooshubham124@gmail.com>
2022-05-14 15:51:31 +0530

    added hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ const ALCHEMY_API_KEY = process.env.API_KEY;
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 module.exports = {
- solidity: "0.8.0",
+ solidity: "0.8.4",
  networks: {
   rinkeby: {
    url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
