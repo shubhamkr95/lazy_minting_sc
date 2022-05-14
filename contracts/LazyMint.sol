25478eec808388c20c234e8ebb1f4e9c3a3b95fc
shubhamkr95 <sahooshubham124@gmail.com>
2022-05-14 15:42:22 +0530

    added minter role, domain and version

--- contracts/LazyMint.sol
+++ contracts/LazyMint.sol
@@ -7,3 +7,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
+contract LazyNFT is ERC721URIStorage, EIP712, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    string private constant SIGNING_DOMAIN = "LazyNFT-Voucher";
+    string private constant SIGNATURE_VERSION = "1";
+
+    mapping(address => uint256) public pendingWithdrawals;
+}
