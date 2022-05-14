c0276922c0a3651cce082d1c9ec2989d7dcdf4ff
shubhamkr95 <sahooshubham124@gmail.com>
2022-05-14 16:27:34 +0530

    modified hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,11 +6,13 @@ require("solidity-coverage");
  */
 
 const API_KEY = process.env.API_KEY;
-const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
  solidity: "0.8.4",
+ defaultNetwork: "hardhat",
  networks: {
+  hardhat: {},
   rinkeby: {
    url: `https://eth-rinkeby.alchemyapi.io/v2/${API_KEY}`,
    accounts: [`${PRIVATE_KEY}`],
