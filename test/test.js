19a94542cdf3b64cd40da43db5b8bb5a8747273f
shubhamkr95 <sahooshubham124@gmail.com>
2022-05-14 16:04:45 +0530

    added deploy fucntion

--- test/test.js
+++ test/test.js
@@ -1,3 +1,17 @@
 const { expect } = require("chai");
 const hardhat = require("hardhat");
 const { ethers } = hardhat;
+const { LazyMinter } = require("../lib/Minter");
+
+async function deploy() {
+ const [minter, redeemer, _] = await ethers.getSigners();
+
+ let factory = await ethers.getContractFactory("LazyNFT", minter);
+ const contract = await factory.deploy();
+
+ return {
+  minter,
+  redeemer,
+  contract,
+ };
+}
