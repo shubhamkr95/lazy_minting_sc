ea0a81ed29d82ea6a8914c476bfb295f18318fad
shubhamkr95 <sahooshubham124@gmail.com>
2022-05-17 14:04:35 +0530

    added file to server.js

--- src/api/connections/database.js
+++ src/api/connections/database.js
@@ -1,16 +1,17 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config({ path: "./../../../.env" });
 
 const mongoose = require("mongoose");
 const { logger } = require("../config/logger.js");
 
-const DB = process.env.DB_URL.replace(
- "<PASSWORD>",
- process.env.DATABASE_PASSWORD
-);
+// const DB = process.env.DB_URL.replace(
+//  "<PASSWORD>",
+//  process.env.DATABASE_PASSWORD
+// );
+
+const DB = process.env.DB_LOCAL;
 
 const dbConnect = mongoose
  .connect(DB)
  .then(() => logger.info("DB connection successful!"));
 
-export default dbConnect;
+module.exports = { dbConnect };
---
+++ src/api/server.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const { logger } = require("./config/logger.js");
+const { AppError } = require("./helpers/responseHandler.js");
+const { dbConnect } = require("./connections/database.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.port || 6000;
+app.listen(port, () => {
+ logger.info(`Connected to port ${port}`);
+});
